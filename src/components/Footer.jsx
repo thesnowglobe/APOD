b74f7b82b3c4b3acc67887b39656eddbc625fed3
m0chi/ <aly0mal99@gmail.com>
2025-02-24 13:49:23 +0100

    template jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
-    <div>
-
-    </div>
+    <footer>
+      <div></div>
+      <div>
+        <h1>APOD PROJECT</h1>
+        <h2>title from data</h2>
+      </div>
+    </footer>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 export default function Main() {
   return (
     <div>
-
+      <img src="" alt=""/>
     </div>
   )
 }
