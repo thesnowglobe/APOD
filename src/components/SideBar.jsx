be4948c4f2177d4155d3d8df0072586a45ab8c3d
m0chi/ <aly0mal99@gmail.com>
2025-02-24 14:25:31 +0100

    add SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,19 @@
-export default function SideBar() {
+/* eslint-disable react/prop-types */
+export default function SideBar(props) {
+  const { handleToggleModal, data } = props
   return (
-    <div>
-
+    <div className="sidebar">
+      <div onClick={handleToggleModal} className="bgOverlay"></div>
+      <div className="sideBarContents">
+        <h2>{data?.title}</h2>
+        <div className="descriptionContainer">
+          <p className="descriptionTitle">{data?.date}</p>
+          <p>{data?.explanation}</p>
+        </div>
+        <button onClick={handleToggleModal}>
+          <i className="fa-solid fa-arrow-right"></i>
+        </button>
+      </div>
     </div>
   )
 }
