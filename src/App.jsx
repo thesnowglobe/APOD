5cb351b00be196150b62321d3ce29a1755304ead
m0chi/ <aly0mal99@gmail.com>
2025-02-24 13:36:25 +0100

    remove default imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import Main from "./components/Main"
+import SideBar from "./components/SideBar"
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <>
+      <Main />
+      <SideBar />
+      <Footer />
+    </>
   )
 }
 
